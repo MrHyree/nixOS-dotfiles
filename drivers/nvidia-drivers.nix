7056dc2894eb3a41e574cefd5bb3595d39e461c4
MrHyree <haypikbrooes@gmail.com>
2026-09-16 23:11:35 +0700

    .

--- drivers/nvidia-drivers.nix
+++ drivers/nvidia-drivers.nix
@@ -16,7 +16,7 @@ in
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
-      modesetting.enable = true;.
+      modesetting.enable = true;
       powerManagement.enable = false;
       open = false;
       nvidiaSettings = true;
